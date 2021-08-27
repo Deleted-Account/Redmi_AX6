7de04551768c7075af0a5a1a41d6e515ab06572b
Deleted-Account <48331538+Deleted-Account@users.noreply.github.com>
2021-08-28 02:50:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,6 +28,9 @@ rm -rf package/lean/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
 rm -rf package/lean/luci-theme-argon
 rm -rf package/kernel/mac80211/files/lib/wifi
+rm -rf package/lean/reclone*
+rm -rf package/lean/pdnsd-alt
+rm -rf package/lean/luci-app-zerotier
 
 # 添加额外非必须软件包
 # git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
