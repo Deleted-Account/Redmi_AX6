ef1e46c901c97d46674db68328ba04477bab914d
Deleted-Account <48331538+Deleted-Account@users.noreply.github.com>
2022-03-13 18:49:55 +0800

    Update diy-part2_test.sh

--- diy-part2_test.sh
+++ diy-part2_test.sh
@@ -91,4 +91,5 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 # svn co https://github.com/Boos4721/openwrt/trunk/tools tools
 
 # Passwall LuCI
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/
+# svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/
